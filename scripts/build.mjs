db1bd3c2f45e5d5c5a258faa1a6d527f0c5996c6
jens <jens@doellmann.com>
2021-06-04 15:55:01 +0200

    update build script

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,25 +1,39 @@
 import fs from 'fs-extra';
 import execa from 'execa';
 
+async function lintCheck() {
+  const { stdout } = await execa('npm', ['run', 'lint-check']);
+  return stdout;
+}
+
+async function build() {
+  const { stdout } = await execa('npm', [
+    'exec',
+    '--',
+    'tsc',
+    '--project',
+    './packages/vue3-form-validation',
+    '--outDir',
+    `./${dirname}/dist`
+  ]);
+  return stdout;
+}
+
 const dirname = 'dist';
 
 await fs.mkdir(dirname, { recursive: true });
 await fs.copyFile('LICENSE', `./${dirname}/LICENSE`);
 await fs.copyFile('README.md', `./${dirname}/README.md`);
 
+console.log('Checking files ...');
+console.log(await lintCheck());
+
 console.log('Building ...');
-
-const { stdout } = await execa('npm', [
-  'run',
-  'build',
-  '--',
-  '--outDir',
-  `./${dirname}/dist`
-]);
-
-console.log(stdout);
+console.log(await build());
 
 await fs.copyFile(
   `packages/vue3-form-validation/package.json`,
   `./${dirname}/package.json`
 );
+
+console.log('Done');
