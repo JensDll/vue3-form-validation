9f0dd8eb5476504fcaae00018a58d26a7816fb07
JensDll <jens@doellmann.com>
2021-06-04 15:32:46 +0200

    fix: $validating false to early for multiple async rules (#136)

--- packages/vue3-form-validation/src/form/FormField.ts
+++ packages/vue3-form-validation/src/form/FormField.ts
@@ -4,40 +4,35 @@ import { jsonCopy } from '../common/json-copy/jsonCopy';
 import { Rule } from '../composition/useValidation';
 
 export class FormField {
-  private errors: (string | null)[];
-  private initialModelValue: any;
+  private _errors: (string | null)[];
+  private _initialModelValue: any;
 
+  name: string;
+  rulesValidating = ref(0);
   modelValue: ReturnType<typeof ref> | ReturnType<typeof reactive>;
   touched = false;
-  validating = ref(false);
-  name: string;
+  errors = computed(() => this._errors.filter(isDefined));
+  validating = computed(() => this.rulesValidating.value > 0);
+  hasError = computed(() => this.errors.value.length > 0);
 
   constructor(name: string, modelValue: any, rules: Rule[]) {
     this.name = name;
-    this.errors = reactive(rules.map(() => null));
+    this._errors = reactive(rules.map(() => null));
 
     if (isRef(modelValue) || isReactive(modelValue)) {
       this.modelValue = modelValue;
-      this.initialModelValue = jsonCopy(unref(modelValue));
+      this._initialModelValue = jsonCopy(unref(modelValue));
     } else if (isObject(modelValue)) {
       this.modelValue = reactive(modelValue);
-      this.initialModelValue = jsonCopy(this.modelValue);
+      this._initialModelValue = jsonCopy(this.modelValue);
     } else {
       this.modelValue = ref(modelValue);
-      this.initialModelValue = jsonCopy(unref(modelValue));
+      this._initialModelValue = jsonCopy(unref(modelValue));
     }
   }
 
   setError(ruleNumber: number, error: string | null) {
-    this.errors[ruleNumber] = error;
-  }
-
-  getErrors() {
-    return computed(() => this.errors.filter(isDefined));
-  }
-
-  hasError() {
-    return computed(() => this.getErrors().value.length > 0);
+    this._errors[ruleNumber] = error;
   }
 
   reset(toDefaultValues: boolean) {
@@ -46,19 +41,19 @@ export class FormField {
     if (toDefaultValues) {
       if (isRef(this.modelValue)) {
         if (isArray(this.modelValue.value)) {
-          this.modelValue.value = jsonCopy(this.initialModelValue);
+          this.modelValue.value = jsonCopy(this._initialModelValue);
         } else {
-          this.modelValue.value = this.initialModelValue;
+          this.modelValue.value = this._initialModelValue;
         }
       } else {
-        const copy = jsonCopy(this.initialModelValue);
+        const copy = jsonCopy(this._initialModelValue);
         Object.assign(this.modelValue, copy);
       }
     }
 
     Object.assign(
-      this.errors,
-      this.errors.map(() => null)
+      this._errors,
+      this._errors.map(() => null)
     );
   }
 }
