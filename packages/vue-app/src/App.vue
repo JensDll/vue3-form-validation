3f1f30e4a7f6edf7da6add78bf8d5de06b4221d2
jens <jens@doellmann.com>
2021-09-20 11:51:56 +0200

    initial commit

---
+++ packages/vue-app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 class="font-bold text-3xl">{{ x }}</h1>
+</template>
+
+<script setup lang="ts">
+const x: number = 20
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- packages/vue-app/src/main.ts
+++ packages/vue-app/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { router } from './router'
 import 'tailwindcss/tailwind.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).mount('#app')
