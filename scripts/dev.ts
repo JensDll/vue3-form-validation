9d95b1a827ba85e0cbeab7bb17866527b78f2262
jens <jens@doellmann.com>
2021-10-04 11:53:40 +0200

    update tsconfig

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -3,7 +3,6 @@ import yargs from 'yargs'
 
 const args = await yargs(process.argv.slice(2)).array('targets').argv
 args['targets'] ||= ['vue3-form-validation']
-console.log(args)
 
 console.log('Running rollup in watch mode ...')
 
@@ -13,7 +12,7 @@ await execa(
     '--config',
     '--watch',
     '--environment',
-    `TARGETS:${args.targets?.join(' ')},FORMATS:esm`
+    `TARGETS:${args.targets?.join(' ')},FORMATS:esm cjs`
   ],
   {
     stdio: 'inherit'
