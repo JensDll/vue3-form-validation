b34e42bcacdfc9d17d94fea5df564b90ea8119fd
jens <jens@doellmann.com>
2021-11-02 22:05:21 +0100

    update dependencies

--- packages/examples/src/domain/typeGuards.ts
+++ packages/examples/src/domain/typeGuards.ts
@@ -1,7 +1,6 @@
 export const guards = {
   isDefined: <T>(x: T | null | undefined): x is T =>
     x !== null && typeof x !== 'undefined',
-  // @ts-ignore save to ignore
-  isFormElement: <T extends HTMLElement>(el: T): el is HTMLFormElement =>
+  isFormElement: (el: HTMLElement): el is HTMLFormElement =>
     el.tagName === 'FORM'
 }
