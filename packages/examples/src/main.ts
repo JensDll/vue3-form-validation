51a3a99df5f24e4be59512d09f7ec82e049f449d
Jens Döllmann <jensdollmann@Jenss-MacBook-Pro.fritz.box>
2021-11-29 16:33:37 +0100

    Update dependencies

--- packages/examples/src/main.ts
+++ packages/examples/src/main.ts
@@ -7,7 +7,7 @@ import App from './App.vue'
 createApp(App).use(router).use(validation).mount('#app')
 
 declare module 'vue3-form-validation' {
-  export interface CustomValidationBehaviorFunctions {
+  interface CustomValidationBehaviorFunctions {
     example: any
   }
 }
