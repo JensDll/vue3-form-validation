67678bc415044d67a86bba46b4eb7d36c38aa0ca
jens <jens@doellmann.com>
2021-12-07 11:43:36 +0100

    Update dependencies

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/packages/**/__tests__/**/*spec.[jt]s?(x)'],
+  modulePathIgnorePatterns: ['<rootDir>/publish'],
   globals: {
     'ts-jest': {
       isolatedModules: true
